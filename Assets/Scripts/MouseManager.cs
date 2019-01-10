b26875342da41849ae82571661310ab9abdf5105
Richard Jalonen <richard.jalonen@tptlive.ee>
2019-01-10 22:16:54 +0200

    initial commit

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    public LayerMask ClickableLayer;
+    public Texture2D NormalCursor;
+    public Texture2D ClickableObjectCursor;
+    public Texture2D DoorwayCursor;
+    public Texture2D CombatCursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycastHit;
+        //When mouseover a layer that should be interactable
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, 50,
+            ClickableLayer.value))
+        {
+            bool door = false;
+            if (raycastHit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(DoorwayCursor, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(ClickableObjectCursor, new Vector2(16, 16), CursorMode.Auto);
+            }
+        }
+        //Non-interactable stuff
+        else
+        {
+            Cursor.SetCursor(NormalCursor, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
